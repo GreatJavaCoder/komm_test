19c50be45ad79b189e6e7faa5f22a286d88c9291
GreatJavaCoder <kandys@yandex.ru>
2023-07-27 19:33:24 +0300

    jdbc commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        testClass.getClass();
+        TestClass.getConnection();
+    }
+}
---
+++ src/main/java/TestClass.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestClass {
+    public static final String url = "jdbc:mysql://localhost:3306/training_db";
+    public static final String user = "root";
+    public static final String pass = "boss1949";
+    public static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException t) {
+            t.printStackTrace();
+            throw new RuntimeException();
+        }
+        return connection;
+    }
+}
